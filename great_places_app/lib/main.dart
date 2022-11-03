d5cee202a14a65ee1f693127e5f1b790cecee8e6
azhaganandhan <anandpec99@gmail.com>
2022-11-03 14:03:22 +0530

    WIP

--- great_places_app/lib/main.dart
+++ great_places_app/lib/main.dart
@@ -1,27 +1,40 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:great_places_app/providers/great_places.dart';
+import 'package:great_places_app/screens/add_place_screen.dart';
+import 'package:great_places_app/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.teal.shade800,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          // buttonColor: Colors.amber,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (context) => AddPlaceScreen(),
+        },
       ),
-      home: GreatPlacesApp(),
     );
   }
 }
 
-class GreatPlacesApp extends StatelessWidget {
-  const GreatPlacesApp({Key key}) : super(key: key);
+// class GreatPlacesApp extends StatelessWidget {
+//   const GreatPlacesApp({Key key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
---
+++ great_places_app/lib/screens/add_place_screen.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:great_places_app/providers/great_places.dart';
+import 'package:great_places_app/widgets/image_input.dart';
+import 'package:provider/provider.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File _pickedImage;
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a New Place'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(_selectImage),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _savePlace,
+            icon: Icon(Icons.add),
+            label: Text('Add Place'),
+            style: ElevatedButton.styleFrom(
+                elevation: 0,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                primary: Theme.of(context).accentColor,
+                onPrimary: Colors.black),
+          )
+        ],
+      ),
+    );
+  }
+}
